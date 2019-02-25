693bd1245ffd267b05a5df2df3a1723f355d55d1
kekoasantana <kekoasantana42@gmail.com>
2019-02-24 15:20:16 -1000

    Added animation feature, still no jump

--- Assets/Scripts/PlatformPlayer.cs
+++ Assets/Scripts/PlatformPlayer.cs
@@ -6,12 +6,14 @@ public class PlatformPlayer : MonoBehaviour
 {
     public float speed = 250.0f;
 
+    private Animator _anim;
     private Rigidbody2D _body;
 
     // Start is called before the first frame update
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,5 +22,10 @@ public class PlatformPlayer : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
+
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
     }
 }
